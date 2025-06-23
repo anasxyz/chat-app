29a44b4548bd23350d8f22c3e3ba96117c3bc7ed
anasxyz <anaswessam176@gmail.com>
2025-06-23 20:13:23 +0100

    In process of user authentication and JWTs

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,9 +1,33 @@
-export const signup = (req, res) => {
-  const{ fullName, email, password } = req.body
+import User from "../models/user.mode.js"
+import bcrypt from "bcryptjs"
+
+export const signup = async (req, res) => {
+  const { fullName, email, password } = req.body
   try {
-    
+    if (password.length < 6) {
+      return res.status(400).json({ message: "Password must be at least 6 characters" })
+    }
+
+    const user = await User.findOne({ email })
+
+    if (user) return res.status(400).json({ message: "Email already exists" })
+
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    const newUser = new User({
+      fullName: fullName,
+      email: email,
+      password: hashedPassword,
+    })
+
+    if (newUser) {
+      // generate jwt token ehre
+    } else {
+      res.status(400).json({ message: "Invalid user data " })
+    }
   } catch (error) {
-    
+
   }
 }
 export const login = (req, res) => {
@@ -11,4 +35,4 @@ export const login = (req, res) => {
 }
 export const logout = (req, res) => {
   res.send("logout route");
-}
+} 
