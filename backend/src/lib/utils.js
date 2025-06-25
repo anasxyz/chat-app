cb1e0537f1e0cb86cde26d278b71e5271a39c3aa
anasxyz <anaswessam176@gmail.com>
2025-06-25 23:35:59 +0100

    Fixed userId variable in token generator

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken"
 
-export const generateToken = (userid, res) => {
+export const generateToken = (userId, res) => {
   const token = jwt.sign({userId}, process.env.JWT_SECRET, { expiresin: "7d" });
 
   res.cookie("jwt", token, { 
