76115d09837423aa60f6ca20d453498f99e9aed7
anasxyz <anaswessam176@gmail.com>
2025-06-25 22:13:09 +0100

    Finished sign up function

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,4 +1,4 @@
-import User from "../models/user.mode.js"
+import User from "../models/user.model.js"
 import bcrypt from "bcryptjs"
 
 export const signup = async (req, res) => {
@@ -23,11 +23,22 @@ export const signup = async (req, res) => {
 
     if (newUser) {
       // generate jwt token ehre
+      generateToken(newUser._id, res)
+      await newUser.save();
+
+      res.status(201).json({
+        _id: newUser._id,
+        fullName: newUser.fullName,
+        email: newUser.email,
+        profilePic: newUser.profilePic,
+      });
     } else {
-      res.status(400).json({ message: "Invalid user data " })
+      res.status(400).json({ message: "Invalid user data " });
     }
   } catch (error) {
+      console.log("Error in signup controller", error.message);
 
+      res.status(500).json({ message: "Internal Server Error " });
   }
 }
 export const login = (req, res) => {
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,1 +1,14 @@
-export const generateToken = () => {}
+import jwt from "jsonwebtoken"
+
+export const generateToken = (userid, res) => {
+  const token = jwt.sign({userId}, process.env.JWT_SECRET, { expiresin: "7d" });
+
+  res.cookie("jwt", token, { 
+    maxAge: 7*24*60*60*1000, // 7 days in ms
+    httpOnly: true, // prevents XSS attacks cross-site scripting
+    sameSite: "strict", // CSRF attacks cross-site request forgery attacks
+    secure: process.env.NODE_ENV !== "development",
+  }); 
+
+  return token;
+}
