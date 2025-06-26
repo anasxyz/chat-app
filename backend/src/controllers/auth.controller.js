90b4f973b9470b4be1e5024ee806de85ea7b7228
anasxyz <anaswessam176@gmail.com>
2025-06-27 00:38:32 +0100

    Added user authentication checking function

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -101,10 +101,19 @@ export const updateProfile = async (req, res) => {
 
     const uploadResponse = await cloudinary.uploader.upload(profilePic);
     const updatedUser = await User.findByIdAndUpdate(userId, { profilePic:uploadResponse.secure_url }, { new:true });
-
+ 
     res.status(200).json(updatedUser);
   } catch (error) {
     console.log("error in update profile: ", error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export const checkAuth = (req, res) => {
+  try {
+    res.status(200).json(req.user);
+  } catch (error) {
+    console.log("Error in checkAuth controller");
     res.status(500).json({ message: "Internal server error" });
   }
 }
