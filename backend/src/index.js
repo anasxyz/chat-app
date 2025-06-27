07f6687493cb7991678c60449c1ac1929df63c73
anasxyz <anaswessam176@gmail.com>
2025-06-28 00:52:44 +0100

    Added user fetching for sidebar

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,6 +5,7 @@ import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 
 import authRoutes from './routes/auth.route.js';
+import messageRoutes from './routes/message.route.js';
 
 const app = express();
 
@@ -14,7 +15,8 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/auth", messageRoutes);
 
 app.listen(5001, () => {
   console.log("server is running on PORT: " + PORT);
