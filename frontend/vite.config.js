e9b81151065f576ff72a2c14c79fcdc8306decf8
anasxyz <anaswessam176@gmail.com>
2025-07-07 17:02:54 +0100

    FINALLY FIXED TAILWIND DAISYUI POSTCSS CONFIG FILE STUFF OH MY GOD

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import daisyui from 'daisyui';
 
 export default defineConfig({
-  plugins: [
-    tailwindcss(),
-    daisyui,
-  ],
-})
+  plugins: [react(), tailwindcss()],
+});
