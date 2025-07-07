2245e77212bc11c72dc5f724a149657bba6c2c14
anasxyz <anaswessam176@gmail.com>
2025-07-07 20:33:36 +0100

    Added password input in form

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useAuthStore } from '../store/useAuthStore';
-import { Mail, MessageSquare, User } from 'lucide-react';
+import { Lock, Mail, MessageSquare, User } from 'lucide-react';
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -67,8 +67,26 @@ const SignUpPage = () => {
                 type="text"
                 className={`input input-bordered w-full pl-10`}
                 placeholder="you@example.com"
-                value={formData.fullName}
+                value={formData.email}
                 onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+              />
+            </div>
+          </div>
+
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text font-medium">Password</span> 
+            </label>
+            <div className="relative">
+              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none z-10">
+                <Lock className="size-5 text-base-content/40" />
+              </div>
+              <input
+                type={showPassword ? "text" : "password"}
+                className={`input input-bordered w-full pl-10`}
+                placeholder="••••••••"
+                value={formData.password}
+                onChange={(e) => setFormData({ ...formData, password: e.target.value })}
               />
             </div>
           </div>
