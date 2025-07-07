3b012ea7834a2e2939ac375d763d10a8d915dcbb
anasxyz <anaswessam176@gmail.com>
2025-07-07 12:51:06 +0100

    Forgot to commmit idek

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useAuthStore } from '../store/auth';
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -8,7 +9,20 @@ const SignUpPage = () => {
     password: "",
   });
 
-  return <div>SignUpPage</div>;
+  const { signup, isSigningup } = useAuthStore();
+
+  const validateForm = () => {
+
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  return <div className="min-h-screen grid lg:grid-cols-2">
+    { /* left side */ }
+    
+  </div>
 }
 
 export default SignUpPage;
