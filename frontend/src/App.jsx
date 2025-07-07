d818b0d9b35b5e747d8f5bdb9d4b94f60b829a7c
anasxyz <anaswessam176@gmail.com>
2025-07-07 22:37:16 +0100

    Using react hot toast

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,6 +12,7 @@ import { useAuthStore } from './store/useAuthStore';
 import { useEffect } from 'react';
 
 import { Loader } from 'lucide-react';
+import { Toaster } from 'react-hot-toast';
 
 const App = () => {
   const { authUser, checkAuth, isCheckingAuth } = useAuthStore();
@@ -37,6 +38,8 @@ const App = () => {
         <Route path="/settings" element={<SettingsPage />} />
         <Route path="/profile" element={!authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
+
+      <Toaster />
     </div>
   );
 };
