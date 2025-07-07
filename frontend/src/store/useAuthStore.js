fb089aaad7fb8d886d4dd812a9e488db211a39d9
anasxyz <anaswessam176@gmail.com>
2025-07-08 00:28:48 +0100

    Added navbar buttons and logout functionality

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -38,7 +38,7 @@ export const useAuthStore = create((set) => ({
 
   logout: async () => {
     try {
-      await axiosInstance.get("/auth/logout");
+      await axiosInstance.post("/auth/logout");
       set({ authUser: null });
       toast.success("Logged out successfully");
     } catch (error) {
