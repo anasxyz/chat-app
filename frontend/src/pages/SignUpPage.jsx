77b7cac6a653b8c99a5fb3cb42ff08a73d3636a7
anasxyz <anaswessam176@gmail.com>
2025-07-07 17:48:36 +0100

    Fixed user icon not showing

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -42,7 +42,7 @@ const SignUpPage = () => {
               <span className="label-text font-medium">Full Name</span> 
             </label>
             <div className="relative">
-              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none z-10">
                 <User className="size-5 text-base-content/40" />
               </div>
               <input
