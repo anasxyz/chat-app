e803ae9b4301841e166f8dd7170eb1cf167c163d
anasxyz <anaswessam176@gmail.com>
2025-07-07 21:34:46 +0100

    Added login link in signup page

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useAuthStore } from '../store/useAuthStore';
 import { Eye, EyeOff, Loader2, Lock, Mail, MessageSquare, User } from 'lucide-react';
+import { Link } from 'react-router-dom';
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -113,6 +114,15 @@ const SignUpPage = () => {
             )}
           </button>
         </form>
+
+        <div className="text-center">
+          <p className="text-base-content/60">
+            Already have an account?{" "}
+            <Link to="/login" className="link link-primary">
+              Sign in
+            </Link>
+          </p>
+        </div>
       </div>
     </div>
   </div>;
